d6609c7da784616bf42aa6a5192231fcdb338b1d
SHShadhin <shshadhin0@gmail.com>
2026-05-17 16:24:37 +0600

    Added animate.css in bookcard

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,42 +1,4 @@
-// 'use client';
-// import { Button, Card, Chip } from '@heroui/react';
-// import Image from 'next/image';
-// import Link from 'next/link';
-// import React from 'react';
-// import { authClient } from '@/lib/auth-client';
 
-// const BookCard = ({ book }) => {
-//   const { data: session } = authClient.useSession();
-
-//   const user = session?.user;
-
-//   console.log(book);
-//   return (
-//     <Card className="border border-yellow-200 shadow-md">
-//       <div className="w-full h-[300px] relative overflow-hidden rounded-lg">
-//         <Image
-//           src={book.image_url}
-//           fill
-//           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-//           className="object-cover"
-//           alt={book.title}
-//         />
-//       </div>
-//       <div className="text-center">
-//         <h2 className="text-xl font-bold text-yellow-600">{book.title}</h2>
-//         <p className="text-lg font-medium text-gray-500">{book.author}</p>
-//       </div>
-//       <Chip size="sm" color="danger" className="absolute right-6 top-6">
-//         {book.category}
-//       </Chip>
-//       <Link href={user ? `/all-books/${book.id}` : '/login'}>
-//         <Button className="w-full bg-yellow-500">View Details</Button>
-//       </Link>
-//     </Card>
-//   );
-// };
-
-// export default BookCard;
 
 'use client';
 
@@ -45,6 +7,7 @@ import { authClient } from '@/lib/auth-client';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { toast } from 'react-toastify';
+import 'animate.css';
 
 const BookCard = ({ book }) => {
   const { data: session } = authClient.useSession();
@@ -66,7 +29,7 @@ const BookCard = ({ book }) => {
   };
 
   return (
-    <Card className="border border-yellow-200 shadow-md">
+    <Card className="border border-yellow-200 shadow-md animate__animated animate__fadeInUp hover:scale-105 transition-transform duration-300">
       <div className="w-full h-[300px] relative overflow-hidden rounded-lg">
         <Image
           src={book.image_url}
